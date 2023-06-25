2b4d4b70a854683e89d5ac5121fd30392e480feb
jsilance <52762222+jsilance@users.noreply.github.com>
2023-06-25 15:05:23 +0200

    full fix, and working

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jusilanc <jusilanc@student.s19.be>         +#+  +:+       +#+        */
+/*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 22:19:03 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/06/24 03:05:08 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/06/25 14:50:50 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,40 +35,45 @@ typedef struct s_circle
 
 static int	zone_init(t_base *base)
 {
-	if (fscanf(base->file, "%i %i %c\n", &base->width, &base->height,
-			&base->bg_char) != 3)
+	if (fscanf(base->file, "%i %i %c", &base->width, &base->height,
+			&base->bg_char) != 3 || base->width <= 0 || base->width > 300
+		|| base->height <= 0 || base->height > 300)
 	{
 		write(1, "Error: Operation file corrupted\n", 32);
 		return (-1);
 	}
+	fscanf(base->file, "\n");
 	return (0);
 }
 
 static void	map_write(t_base *base, char *ptr)
 {
-	int	i;
+	int	y;
 
-	i = 0;
-	while (i < base->height)
+	y = 0;
+	while (y < base->height)
 	{
-		write(1, &ptr[i * base->width], base->width);
+		write(1, &ptr[y * base->width], base->width);
 		write(1, "\n", 1);
-		i++;
+		y++;
 	}
 }
 
 static void	map_init(t_base *base, char *ptr)
 {
-	int	i;
-	int	j;
+	int	y;
+	int	x;
 
-	i = 0;
-	while (i < base->height)
+	y = 0;
+	while (y < base->height)
 	{
-		j = 0;
-		while (j < base->width)
-			ptr[(i * base->width) + j++] = base->bg_char;
-		i++;
+		x = 0;
+		while (x < base->width)
+		{
+			ptr[y * base->width + x] = base->bg_char;
+			x++;
+		}
+		y++;
 	}
 }
 
@@ -86,11 +91,10 @@ static void	circle_draw(t_base *base, t_circle *circle, char *map)
 		{
 			dist = sqrtf((x - circle->xpos) * (x - circle->xpos) + (y
 						- circle->ypos) * (y - circle->ypos));
-			if (circle->type == 'c' && circle->radius - dist >= 0
-				&& circle->radius - dist < 1)
-				map[(y * base->width) + x] = circle->c_char;
-			else if (circle->type == 'C' && dist <= circle->radius)
-				map[(y * base->width) + x] = circle->c_char;
+			if (circle->type == 'C' && dist <= circle->radius)
+				map[y * base->width + x] = circle->c_char;
+			else if (circle->type == 'c' && circle->radius - dist < 1 && circle->radius - dist >= 0)
+				map[y * base->width + x] = circle->c_char;
 			x++;
 		}
 		y++;
@@ -103,31 +107,24 @@ int	main(int argc, char **argv)
 	t_circle	circle;
 	char		*map;
 
-	base.size = 0;
 	if (argc != 2)
 	{
 		write(1, "Error: argument\n", 16);
 		return (1);
 	}
 	base.file = fopen(argv[1], "r");
-	if (!base.file)
-	{
-		write(1, "Error: Operation file corrupted\n", 32);
-		return (1);
-	}
 	if (zone_init(&base) == -1)
 		return (1);
 	map = malloc(sizeof(char) * base.width * base.height + 1);
 	map_init(&base, map);
 	while (1)
 	{
-		base.size = fscanf(base.file, "%c %f %f %f %c", &circle.type,
-				&circle.xpos, &circle.ypos, &circle.radius, &circle.c_char);
+		base.size = fscanf(base.file, "%c %f %f %f %c", &circle.type, &circle.xpos, &circle.ypos, &circle.radius, &circle.c_char);
 		fscanf(base.file, "\n");
-		if ((base.size != 5 && base.size != -1) || (circle.type != 'c'
-				&& circle.type != 'C'))
+		if ((base.size != 5 && base.size != -1) || (base.size == 5 && circle.type != 'c' && circle.type != 'C') || (base.size != -1 && circle.radius <= 0))
 		{
 			free(map);
+			fclose(base.file);
 			write(1, "Error: Operation file corrupted\n", 32);
 			return (1);
 		}
